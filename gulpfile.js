9951ac01ae780871b53f2a290a1ec90028b6f5cf
Luong Van Kiet <luongvankiet.97@gmail.com>
2022-10-06 14:09:59 +1100

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
 function buildStyles() {
-    return src('sass/index.scss')
+    return src('./sass/**/*.scss')
         .pipe(sass())
-        .pipe(dest('css'))
+        .pipe(dest('./css'))
 }
 
 function watchTask() {
-    watch(['index.scss'], buildStyles)
+    watch(['./sass/**/*.scss'], buildStyles)
 }
 
-exports.default = series(buildStyles, watchTask);
+exports.default = series(buildStyles, watchTask)
