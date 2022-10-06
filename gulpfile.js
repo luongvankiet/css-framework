bb60852b930589b62bbeb51f22402d97ee0f1e22
Luong Van Kiet <luongvankiet.97@gmail.com>
2022-10-06 14:16:26 +1100

    refactor folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
 function buildStyles() {
-    return src('./sass/**/*.scss')
+    return src('./assets/sass/**/*.scss')
         .pipe(sass())
-        .pipe(dest('./css'))
+        .pipe(dest('./assets/dist/css'))
 }
 
 function watchTask() {
-    watch(['./sass/**/*.scss'], buildStyles)
+    watch(['./assets/sass/**/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
